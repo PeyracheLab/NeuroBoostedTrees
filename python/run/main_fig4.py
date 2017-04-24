60c6323a5ccdd4e955cc8b442f89ec20c476b14a
gviejo <guillaume.viejo@gmail.com>
2017-04-24 15:10:18 -0400

    figure colors

--- python/run/main_fig4.py
+++ python/run/main_fig4.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 '''
-    File name: main_fig4.py
-    Author: Guillaume Viejo
-    Date created: 30/03/2017    
-    Python Version: 2.7
+	File name: main_fig4.py
+	Author: Guillaume Viejo
+	Date created: 30/03/2017    
+	Python Version: 2.7
 
 
 '''
@@ -74,48 +74,48 @@ def tuning_curve(x, f, nb_bins):
 	return (x, tcurve)
 
 def test_features(features, targets, learners = ['glm_pyglmnet', 'nn', 'xgb_run', 'ens']):
-    '''
-        Main function of the script
-        Return : dictionnary with for each models the score PR2 and Yt_hat
-    '''
-    X = data[features].values
-    Y = np.vstack(data[targets].values)
-    Models = {method:{'PR2':[],'Yt_hat':[]} for method in learners}
-    learners_ = list(learners)
-    print learners_
+	'''
+		Main function of the script
+		Return : dictionnary with for each models the score PR2 and Yt_hat
+	'''
+	X = data[features].values
+	Y = np.vstack(data[targets].values)
+	Models = {method:{'PR2':[],'Yt_hat':[]} for method in learners}
+	learners_ = list(learners)
+	print learners_
 
-    # Special case for glm_pyglmnet to go parallel
-    if 'glm_pyglmnet' in learners:
-        print('Running glm_pyglmnet...')                
-        # # Map the targets for 6 cores by splitting Y in 6 parts
-        pool = multiprocessing.Pool(processes = 6)
-        value = pool.map(glm_parallel, itertools.izip(range(6), itertools.repeat(X), itertools.repeat(Y)))
-        Models['glm_pyglmnet']['Yt_hat'] = np.vstack([value[i]['Yt_hat'] for i in xrange(6)]).transpose()
-        Models['glm_pyglmnet']['PR2'] = np.vstack([value[i]['PR2'] for i in xrange(6)])     
-        learners_.remove('glm_pyglmnet')        
+	# Special case for glm_pyglmnet to go parallel
+	if 'glm_pyglmnet' in learners:
+		print('Running glm_pyglmnet...')                
+		# # Map the targets for 6 cores by splitting Y in 6 parts
+		pool = multiprocessing.Pool(processes = 6)
+		value = pool.map(glm_parallel, itertools.izip(range(6), itertools.repeat(X), itertools.repeat(Y)))
+		Models['glm_pyglmnet']['Yt_hat'] = np.vstack([value[i]['Yt_hat'] for i in xrange(6)]).transpose()
+		Models['glm_pyglmnet']['PR2'] = np.vstack([value[i]['PR2'] for i in xrange(6)])     
+		learners_.remove('glm_pyglmnet')        
 
-    for i in xrange(Y.shape[1]):
-        y = Y[:,i]
-        # TODO : make sure that 'ens' is the last learner
-        for method in learners_:
-            if method != 'ens': # FIRST STAGE LEARNING          
-                print('Running '+method+'...')                              
-                Yt_hat, PR2 = fit_cv(X, y, algorithm = method, n_cv=8, verbose=1)       
-                Models[method]['Yt_hat'].append(Yt_hat)
-                Models[method]['PR2'].append(PR2)           
+	for i in xrange(Y.shape[1]):
+		y = Y[:,i]
+		# TODO : make sure that 'ens' is the last learner
+		for method in learners_:
+			if method != 'ens': # FIRST STAGE LEARNING          
+				print('Running '+method+'...')                              
+				Yt_hat, PR2 = fit_cv(X, y, algorithm = method, n_cv=8, verbose=1)       
+				Models[method]['Yt_hat'].append(Yt_hat)
+				Models[method]['PR2'].append(PR2)           
 
-            elif method == 'ens': # SECOND STAGE LEARNING
-                X_ens = np.transpose(np.array([Models[m]['Yt_hat'][i] for m in learners if m != method]))
-                #We can use XGBoost as the 2nd-stage model
-                Yt_hat, PR2 = fit_cv(X_ens, y, algorithm = 'xgb_run', n_cv=8, verbose=1)        
-                Models['ens']['Yt_hat'].append(Yt_hat)      
-                Models['ens']['PR2'].append(PR2)                        
+			elif method == 'ens': # SECOND STAGE LEARNING
+				X_ens = np.transpose(np.array([Models[m]['Yt_hat'][i] for m in learners if m != method]))
+				#We can use XGBoost as the 2nd-stage model
+				Yt_hat, PR2 = fit_cv(X_ens, y, algorithm = 'xgb_run', n_cv=8, verbose=1)        
+				Models['ens']['Yt_hat'].append(Yt_hat)      
+				Models['ens']['PR2'].append(PR2)                        
 
-    for m in Models.iterkeys():
-        Models[m]['Yt_hat'] = np.array(Models[m]['Yt_hat'])
-        Models[m]['PR2'] = np.array(Models[m]['PR2'])
-        
-    return Models
+	for m in Models.iterkeys():
+		Models[m]['Yt_hat'] = np.array(Models[m]['Yt_hat'])
+		Models[m]['PR2'] = np.array(Models[m]['PR2'])
+		
+	return Models
 
 
 #####################################################################
@@ -138,8 +138,8 @@ for n in ['Pos', 'ADn']:
 ########################################################################
 # MAIN LOOP FOR R2
 ########################################################################
-methods = ['xgb_run', 'nn', 'lin_comb']
-final_data = {}
+# methods = ['xgb_run', 'lin_comb']
+# final_data = {}
 # for g in combination.iterkeys():
 # 	final_data[g] = {}
 # 	for w in combination[g].iterkeys():
@@ -150,149 +150,174 @@ final_data = {}
 # 		    results = test_features(features, targets, methods)
 # 		    final_data[g][w][k] = results
 
-with open("../data/fig4.pickle", 'rb') as f:
-	final_data = pickle.load(f)
+# with open("../data/fig4.pickle", 'wb') as f:
+#   pickle.dump(final_data, f)
+
+# with open("../data/fig4.pickle", 'rb') as f:
+# 	final_data = pickle.load(f)
 
 #####################################################################
 # LEARNING XGB
 #####################################################################
 
-# bsts = {}
-# params = {'objective': "count:poisson", #for poisson output
-#     'eval_metric': "logloss", #loglikelihood loss
-#     'seed': 2925, #for reproducibility
-#     'silent': 1,
-#     'learning_rate': 0.05,
-#     'min_child_weight': 2, 'n_estimators': 580,
-#     'subsample': 0.6, 'max_depth': 5, 'gamma': 0.4}        
-# num_round = 100
+bsts = {}
+params = {'objective': "count:poisson", #for poisson output
+	'eval_metric': "logloss", #loglikelihood loss
+	'seed': 2925, #for reproducibility
+	'silent': 1,
+	'learning_rate': 0.05,
+	'min_child_weight': 2, 'n_estimators': 580,
+	'subsample': 0.6, 'max_depth': 400, 'gamma': 0.4}    
+num_round = 400
 
-# for g in combination.iterkeys():
-# 	bsts[g] = {}
-# 	for w in combination[g].iterkeys():
-# 		bsts[g][w] = {}
-# 		for k in combination[g][w].iterkeys():
-# 			features = combination[g][w][k]['features']
-# 			targets =  combination[g][w][k]['targets']	
-# 			X = data[features].values
-# 			Yall = data[targets].values		
-# 			dtrain = xgb.DMatrix(X, label=Yall)
-# 			bst = xgb.train(params, dtrain, num_round)
-# 			bsts[g][w][k] = bst
+for g in combination.iterkeys():
+	bsts[g] = {}
+	for w in combination[g].iterkeys():
+		bsts[g][w] = {}
+		for k in combination[g][w].iterkeys():
+			features = combination[g][w][k]['features']
+			targets =  combination[g][w][k]['targets']	
+			X = data[features].values
+			Yall = data[targets].values		
+			dtrain = xgb.DMatrix(X, label=Yall)
+			bst = xgb.train(params, dtrain, num_round)
+			bsts[g][w][k] = bst
 
 # #####################################################################
 # # TUNING CURVE
 # #####################################################################
-# all_neurons = [i for i in list(data) if i.split(".")[0] in ['Pos', 'ADn']]
-# X = data['ang'].values
-# Yall = data[all_neurons].values
-# tuningc = {all_neurons[i]:tuning_curve(X, Yall[:,i], nb_bins = 100) for i in xrange(Yall.shape[1])}
+all_neurons = [i for i in list(data) if i.split(".")[0] in ['Pos', 'ADn']]
+X = data['ang'].values
+Yall = data[all_neurons].values
+tuningc = {all_neurons[i]:tuning_curve(X, Yall[:,i], nb_bins = 100) for i in xrange(Yall.shape[1])}
 
 
 #####################################################################
 # EXTRACT TREE STRUCTURE
 #####################################################################
-# thresholds = {}
-# for g in combination.iterkeys():
-# 	thresholds[g] = {}
-# 	for w in combination[g].iterkeys():
-# 		thresholds[g][w] = {}
-# 		for k in combination[g][w].iterkeys():
-# 			thresholds[g][w][k] = extract_tree_threshold(bsts[g][w][k])		
+thresholds = {}
+for g in combination.iterkeys():
+	thresholds[g] = {}
+	for w in combination[g].iterkeys():
+		thresholds[g][w] = {}
+		for k in combination[g][w].iterkeys():
+			thresholds[g][w][k] = extract_tree_threshold(bsts[g][w][k])		
 
-# # need to sort the features by the number of splits
-# sorted_features = dict.fromkeys(thresholds)
-# for k in sorted_features.iterkeys():
-# 	count = np.array([len(thresholds[k][f]) for f in thresholds[k].iterkeys()])
-# 	name = np.array([combination[k]['features'][int(i[1:])] for i in thresholds[k].iterkeys()])
-# 	sorted_features[k] = [name[np.argsort(count)], np.sort(count)]
+# need to sort the features by the number of splits
+sorted_features = {}
+for g in combination.iterkeys():
+	sorted_features[g] = {}
+	for w in combination[g].iterkeys():
+		sorted_features[g][w] = {}
+		for k in combination[g][w].iterkeys():
+			count = np.array([len(thresholds[g][w][k][f]) for f in thresholds[g][w][k].iterkeys()])
+			name = np.array([combination[g][w][k]['features'][int(f[1:])] for f in thresholds[g][w][k].iterkeys()])            
+			sorted_features[g][w][k] = np.array([name[np.argsort(count)], np.sort(count)])
 
-# # matrix relation of splits between neurons
-# relation = {}
-# for k in ['Pos', 'ADn']:
-# 	keys = [i for i in thresholds.iterkeys() if i.split(".")[0] == k]
-# 	tmp = np.zeros((len(keys), len(keys)))
-# 	for l in keys:
-# 		for m,n in zip(sorted_features[l][0], sorted_features[l][1]):
-# 			i = int(l.split(".")[1])
-# 			j = int(m.split(".")[1])
-# 			tmp[i, j] = n
-# 	relation[k] = tmp
-# 	#let's normalize the matrix
-# 	relation[k] = relation[k]/np.max(relation[k])
+# number of splits versus number of individuals value for each neurons
+splitvar = {}
+plotsplitvar = {}
+for g in combination.iterkeys():
+	splitvar[g] = {}
+	plotsplitvar[g] = {}
+	for w in combination[g].iterkeys():
+		splitvar[g][w] = {}
+		plotsplitvar[g][w] = {'nsplit':[], 'unique':[]}
+		for k in combination[g][w].iterkeys():
+			count = [len(np.unique(data[n].values)) for n in sorted_features[g][w][k][0]]
+			splitvar[g][w][k] = np.array([count, sorted_features[g][w][k][1]]).astype(int)
+			plotsplitvar[g][w]['unique'].append(count)
+			plotsplitvar[g][w]['nsplit'].append(sorted_features[g][w][k][1].astype('int'))
+		plotsplitvar[g][w]['unique'] = np.array(plotsplitvar[g][w]['unique']).flatten()
+		plotsplitvar[g][w]['nsplit'] = np.array(plotsplitvar[g][w]['nsplit']).flatten()
+		
+
+
+
 
 #####################################################################
 # PLOTTING
 #####################################################################
 def figsize(scale):
-    fig_width_pt = 483.69687                         # Get this from LaTeX using \the\textwidth
-    inches_per_pt = 1.0/72.27                       # Convert pt to inch
-    golden_mean = (np.sqrt(5.0)-1.0)/2.0            # Aesthetic ratio (you could change this)
-    fig_width = fig_width_pt*inches_per_pt*scale    # width in inches
-    fig_height = fig_width*golden_mean              # height in inches
-    fig_size = [fig_width,fig_height]
-    return fig_size
+	fig_width_pt = 483.69687                         # Get this from LaTeX using \the\textwidth
+	inches_per_pt = 1.0/72.27                       # Convert pt to inch
+	golden_mean = (np.sqrt(5.0)-1.0)/2.0            # Aesthetic ratio (you could change this)
+	fig_width = fig_width_pt*inches_per_pt*scale    # width in inches
+	fig_height = fig_width*golden_mean * 0.5              # height in inches
+	fig_size = [fig_width,fig_height]
+	return fig_size
 
 def simpleaxis(ax):
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.get_xaxis().tick_bottom()
-    ax.get_yaxis().tick_left()
-    # ax.xaxis.set_tick_params(size=6)
-    # ax.yaxis.set_tick_params(size=6)
+	ax.spines['top'].set_visible(False)
+	ax.spines['right'].set_visible(False)
+	ax.get_xaxis().tick_bottom()
+	ax.get_yaxis().tick_left()
+	# ax.xaxis.set_tick_params(size=6)
+	# ax.yaxis.set_tick_params(size=6)
 
+def myticks(x,pos):
+    if x == 0: return "$0$"
+    exponent = int(np.log10(x))
+    coeff = x/10**exponent
+    return r"${:2.0f} \times 10^{{ {:2d} }}$".format(coeff,exponent)
 
 import matplotlib as mpl
 
 mpl.use("pdf")
+
+
+
 pdf_with_latex = {                      # setup matplotlib to use latex for output
-    "pgf.texsystem": "pdflatex",        # change this if using xetex or lautex
-    "text.usetex": True,                # use LaTeX to write all text
-    "font.family": "serif",
-    "font.serif": [],                   # blank entries should cause plots to inherit fonts from the document
-    "font.sans-serif": [],
-    "font.monospace": [],
-    "axes.labelsize": 10,               # LaTeX default is 10pt font.
-    "font.size": 8,
-    "legend.fontsize": 7,               # Make the legend/label fonts a little smaller
-    "xtick.labelsize": 7,
-    "ytick.labelsize": 7,
-    "figure.figsize": figsize(1),     # default fig size of 0.9 textwidth
-    "pgf.preamble": [
-        r"\usepackage[utf8x]{inputenc}",    # use utf8 fonts becasue your computer can handle it :)
-        r"\usepackage[T1]{fontenc}",        # plots will be generated using this preamble
-        ],
-    "lines.markeredgewidth" : 0.2,
-    "axes.linewidth"        : 0.5,
-    "ytick.major.size"      : 1.5,
-    "xtick.major.size"      : 1.5
-    }    
+	"pgf.texsystem": "pdflatex",        # change this if using xetex or lautex
+	"text.usetex": True,                # use LaTeX to write all text
+	"font.family": "serif",
+	"font.serif": [],                   # blank entries should cause plots to inherit fonts from the document
+	"font.sans-serif": [],
+	"font.monospace": [],
+	"axes.labelsize": 6,               # LaTeX default is 10pt font.
+	"font.size": 7,
+	"legend.fontsize": 6,               # Make the legend/label fonts a little smaller
+	"xtick.labelsize": 6,
+	"ytick.labelsize": 6,
+	"figure.figsize": figsize(1),     # default fig size of 0.9 textwidth
+	"pgf.preamble": [
+		r"\usepackage[utf8x]{inputenc}",    # use utf8 fonts becasue your computer can handle it :)
+		r"\usepackage[T1]{fontenc}",        # plots will be generated using this preamble
+		],
+	"lines.markeredgewidth" : 0.2,
+	"axes.linewidth"        : 0.5,
+	"ytick.major.size"      : 1.5,
+	"xtick.major.size"      : 1.5
+	}    
 mpl.rcParams.update(pdf_with_latex)
 import matplotlib.gridspec as gridspec
 from matplotlib.pyplot import *
 
 
 labels = {'mb_10':'MB \n 10 bins', 
-            'mb_60':'MB \n 60 bins', 
-            'mb_360':'MB \n 360 bins', 
-            'lin_comb':'Lin', 
-            'nn':'NN', 
-            'xgb_run':'XGB'}
+			'mb_60':'MB \n 60 bins', 
+			'mb_360':'MB \n 360 bins', 
+			'lin_comb':'Lin', 
+			'nn':'NN', 
+			'xgb_run':'XGB'}
 
 colors_ = {'ADn':'#134B64', 'Pos':'#F5A21E'}
 labels_plot = [labels[m] for m in methods[0:-1]]
 
-figure(figsize = figsize(0.5))
-
+figure(figsize = figsize(1))
+subplots_adjust(hspace = 0.4, wspace = 0.4)
+outer = gridspec.GridSpec(1,3)
+# SUBPLOT 1 ################################################################
+gs = gridspec.GridSpecFromSubplotSpec(1,1,subplot_spec = outer[0])
+subplot(gs[0])
 simpleaxis(gca())
 y = []
 err = []
 x = [0.0]
 color = []
-
-for g in ['peer', 'cros']:
-	for m in ['xgb_run', 'nn']:
-		for k in ['ADn', 'Pos']:			
+for k in ['ADn', 'Pos']:            
+	for g in ['peer', 'cros']:
+		for m in ['xgb_run']:		
 			PR2_art = []    		
 			color.append(colors_[k])
 			for n in final_data[k][g].iterkeys():
@@ -303,31 +328,69 @@ for g in ['peer', 'cros']:
 		x[-1] += 0.3
 	x[-1] += 0.5
 		
-x = np.array(x)
+x = np.array(x)[0:-1]
 y = np.array(y)
 err = np.array(err)		
-x_adn = x[0:-1][np.arange(0, len(y),2)]
-y_adn = y[np.arange(0, len(y),2)]
-e_adn = err[np.arange(0, len(y),2)]
-x_pos = x[0:-1][np.arange(1, len(y),2)]
-y_pos = y[np.arange(1, len(y),2)]
-e_pos = err[np.arange(1, len(y),2)]
+# x_adn = x[0:-1][np.arange(0, len(y),2)]
+# y_adn = y[np.arange(0, len(y),2)]
+# e_adn = err[np.arange(0, len(y),2)]
+# x_pos = x[0:-1][np.arange(1, len(y),2)]
+# y_pos = y[np.arange(1, len(y),2)]
+# e_pos = err[np.arange(1, len(y),2)]
 
-bar(x_adn, y_adn, 0.4, align='center',
-            ecolor='k', color = colors_['ADn'], alpha=.9, ec='w', yerr=e_adn, label = 'Antero Dorsal nucleus')
-bar(x_pos, y_pos, 0.4, align='center',
-            ecolor='k', color = colors_['Pos'], alpha=.9, ec='w', yerr=e_pos, label = 'Post Subiculum')
-            
-plot(x[0:-1], y, 'k.', markersize=3)         
+bar(x[0:2], y[0:2], 0.4, align='center',
+			ecolor='k', color = colors_['ADn'], alpha=.9, ec='w', yerr=err[0:2], label = 'Antero Dorsal nucleus')
+bar(x[2:4], y[2:4], 0.4, align='center',
+			ecolor='k', color = colors_['Pos'], alpha=.9, ec='w', yerr=err[2:4], label = 'Post Subiculum')
+			
+plot(x, y, 'k.', markersize=3)         
 
-xlim(np.min(x)-0.5, np.max(x[0:-1])+0.5)
-ylabel('Pseudo-R2')
-xticks(x_adn+0.2, ['XGB', 'NN']*2)
+# xlim(np.min(x)-0.5, np.max(x[0:-1])+0.5)
+ylabel('Pseudo-R2 (XGBoost)')
+xticks(x, 
+	["ADn $\Rightarrow$ ADn", "Post-S $\Rightarrow$ ADn", "Post-S $\Rightarrow$ Post-S", "ADn $\Rightarrow$ Post-S"], 
+	rotation = 30, 
+	ha = 'right'
+	)
 
-legend(bbox_to_anchor=(0.5, 1.2), loc='upper center', ncol=2, frameon = False)
+legend(bbox_to_anchor=(0.5, 1.2), loc='upper center', ncol=1, frameon = False)
 
-figtext(0.2, -0.2, "ADn $\Rightarrow$ ADn \n Post-S $\Rightarrow$ Post-S \n \scriptsize{(Features $\Rightarrow$ Target)}")
-figtext(0.6, -0.14, "ADn $\Rightarrow$ Post-S \n Post-S $\Rightarrow$ ADn")
+# figtext(0.2, -0.2, "ADn $\Rightarrow$ ADn \n Post-S $\Rightarrow$ Post-S \n \scriptsize{(Features $\Rightarrow$ Target)}")
+# figtext(0.6, -0.14, "ADn $\Rightarrow$ Post-S \n Post-S $\Rightarrow$ ADn")
+
+# SUBPLOT 2 ################################################################
+gs = gridspec.GridSpecFromSubplotSpec(2,2,subplot_spec = outer[1], hspace = 0.9, wspace = 0.9)
+matplotlib.rcParams.update({"axes.labelsize": 	4,
+							"font.size": 		4,
+							"legend.fontsize": 	4,
+							"xtick.labelsize": 	4,
+							"ytick.labelsize": 	4,   
+							})               # Make the legend/label fonts a little smaller
+							
+count = 0
+for g in plotsplitvar.keys():
+	for w in ['peer', 'cros']:
+		subplot(gs[count])
+		simpleaxis(gca())
+		plot(plotsplitvar[g][w]['unique'], plotsplitvar[g][w]['nsplit'], 'o', color = colors_[g], markersize = 1)
+		locator_params(nbins=2)	
+		count += 1
+		
+		ticklabel_format(style='sci', axis='x', scilimits=(0,0), fontsize = 4)
+		ticklabel_format(style='sci', axis='y', scilimits=(0,0), fontsize = 4)
+		xticks(fontsize = 4)
+		yticks(fontsize = 4)
+		xlabel("Number of values", fontsize = 4)
+		ylabel("Number of splits", fontsize = 4)
+
+
+
+# SUBPLOT 3 ################################################################
+gs = gridspec.GridSpecFromSubplotSpec(2,2,subplot_spec = outer[2])
+subplot(gs[0])
+subplot(gs[1])
+subplot(gs[2])
+subplot(gs[3])
 
 savefig("../../figures/fig4.pdf", dpi=900, bbox_inches = 'tight', facecolor = 'white')
 os.system("evince ../../figures/fig4.pdf &")
